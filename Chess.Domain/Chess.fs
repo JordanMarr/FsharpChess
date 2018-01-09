bc7bbe6b0c7f8a377203135241ed517e7b6e1d77
Jordan Marr <jnmarr@hotmail.com>
2018-01-08 19:48:26 -0500

    Renamed for clarity

--- Chess.Domain/Chess.fs
+++ Chess.Domain/Chess.fs
@@ -105,35 +105,35 @@ module Implementation =
     let validateMoveShape (gameState: GameState) (move: ValidatedMoveFrom) =
         let fromPiece, fromCell, toCell = move
                 
-        let x = getHorizDist fromCell toCell
-        let y = getVertDist fromCell toCell
+        let xDelta = getHorizDist fromCell toCell
+        let yDelta = getVertDist fromCell toCell
         
         let validateKnight fromPiece toPieceOption =
-            let isL = match (abs x, abs y) with | (1,2) -> true | (2,1) -> true | _ -> false
+            let isL = match (abs xDelta, abs yDelta) with | (1,2) -> true | (2,1) -> true | _ -> false
             if isL 
             then Valid move
             else Invalid "Knight can only move in an L pattern"
 
         let validateRook (fromColor, fromRank) toPieceOption =
-            let isUpDownLeftRight = (abs x > 0 && y = 0) || (x = 0 && abs y > 0)
+            let isUpDownLeftRight = (abs xDelta > 0 && yDelta = 0) || (xDelta = 0 && abs yDelta > 0)
             if isUpDownLeftRight 
             then Valid move
             else Invalid "Rook can only move up, down, left or right"
 
         let validateBishop (fromColor, fromRank) toPieceOption =
-            let isDiag = (abs x = abs y)
+            let isDiag = (abs xDelta = abs yDelta)
             if isDiag 
             then Valid move
             else Invalid "Bishop can only move diagonally"
 
         let validateKing (fromColor, fromRank) toPieceOption =
-            let isAnyDirectionOneSpace = (abs x = 1 || x = 0) && (abs y = 1 || y = 0)
+            let isAnyDirectionOneSpace = (abs xDelta = 1 || xDelta = 0) && (abs yDelta = 1 || yDelta = 0)
             if isAnyDirectionOneSpace 
             then Valid move
             else Invalid "King can only move one space in any direction"
 
         let validateQueen (fromColor, fromRank) toPieceOption =
-            let isAnyDirection = (abs x = abs y) || (abs x > 0 && y = 0) || (x = 0 && abs y > 0)
+            let isAnyDirection = (abs xDelta = abs yDelta) || (abs xDelta > 0 && yDelta = 0) || (xDelta = 0 && abs yDelta > 0)
             if isAnyDirection 
             then Valid move
             else Invalid "Queen can only move diagonally, up, down, left or right"
@@ -142,7 +142,7 @@ module Implementation =
             match toPieceOption with
             | Some toPiece -> // Moving to an occupied cell
                 // Check for diagonal captures
-                match (fromColor, x, y) with
+                match (fromColor, xDelta, yDelta) with
                 | (White, 1, 1) -> Valid move
                 | (White, -1, 1) -> Valid move
                 | (Black, 1, -1) -> Valid move
@@ -150,7 +150,7 @@ module Implementation =
                 | _ -> Invalid "Pawn can only capture moving one space diagonally"
             | None ->  // Moving to an empty cell
                 // Check for straight non-captures
-                match (fromColor, x, y, pawn) with
+                match (fromColor, xDelta, yDelta, pawn) with
                 | (White, 0, 1, _) -> Valid move            // can always move forward one space to an empty cell
                 | (White, 0, 2, NotMoved) -> Valid move     // can move forward two spaces only if pawn has not yet moved
                 | (Black, 0, -1, _) -> Valid move           // can always move forward one space to an empty cell
