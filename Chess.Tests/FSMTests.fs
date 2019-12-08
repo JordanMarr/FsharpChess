244220830f4e982bb096979804242a4d2494dbfd
Jordan Marr <jmarr@microdesk.com>
2019-12-08 18:24:07 -0500

    Added another unit test without FSM.

--- Chess.Tests/FSMTests.fs
+++ Chess.Tests/FSMTests.fs
@@ -9,21 +9,29 @@ open FsUnit
 module FSMTests =
 
     let cell = Api.deserializeCoord
-    let move c1 c2 = Move { FromCell = cell c1; ToCell = cell c2 }
+    let attemptedMove c1 c2 = { FromCell = cell c1; ToCell = cell c2 }
+    let moveEvent c1 c2 = Move (attemptedMove c1 c2)
 
     [<Test>]
     let ``Test FSM`` () =
         let state =
             NewGame
             |> update Initialize
-            |> update (move "A2" "A4")
-            |> update (move "B7" "B5")
-            |> update (move "A4" "B5")
+            |> update (moveEvent "A2" "A4")
+            |> update (moveEvent "B7" "B5")
+            |> update (moveEvent "A4" "B5")
         
         match state with
         | InProgress gs ->
-            let piece = gs.Board.Item(cell "B5")
-            piece |> should equal (Some (White, Pawn Moved))
+            gs.Board.Item(cell "B5") |> should equal (Some (White, Pawn Moved))
         | _ ->
             failwith "Unexpected state"
 
+    [<Test>]
+    let ``Test without FSM``() =
+        let gs = Implementation.initGame()
+        let gs = Implementation.move gs (attemptedMove "A2" "A4")
+        let gs = Implementation.move gs (attemptedMove "B7" "B5")
+        let gs = Implementation.move gs (attemptedMove "A4" "B5")
+
+        gs.Board.Item(cell "B5") |> should equal (Some (White, Pawn Moved))
